d0946a15445a73d8f82227e9067d82486ca8bd31
Stig Thormodsrud <stig@platinasystems.com>
2019-04-30 17:31:26 -0700

    re-enable blackhole test
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>

--- static.go
+++ static.go
@@ -231,7 +231,7 @@ type staticBlackhole struct{ *docker.Docket }
 func (staticBlackhole) String() string { return "blackhole" }
 
 func (static staticBlackhole) Test(t *testing.T) {
-	if true || testing.Short() || *test.DryRun {
+	if testing.Short() || *test.DryRun {
 		t.SkipNow()
 	}
 

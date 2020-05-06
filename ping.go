ffdaf368bbbdb0408de43c3131e7c728aaf6ef05
Stig Thormodsrud <stig@platinasystems.com>
2020-05-05 18:02:48 -0700

    uncomment test that no longer crashes
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>

--- ping.go
+++ ping.go
@@ -86,8 +86,7 @@ func (list pingFlood) Test(t *testing.T) {
 	if testing.Short() || *Flood <= 0 {
 		t.SkipNow()
 	}
-	t.Log("Skipping because it fails and crashes cpu")
-	t.SkipNow()
+
 	assert := test.Assert{t}
 	nd := []netport.NetDev(list)[0]
 	ns := nd.Netns

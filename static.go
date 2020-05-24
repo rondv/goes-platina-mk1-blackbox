b14f95406239e14c47c5a3704abcdb6fb3231dcc
Stig Thormodsrud <stig@platinasystems.com>
2020-05-24 12:35:38 -0700

    update frr now that goes supports replace blackhole
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>

--- static.go
+++ static.go
@@ -251,10 +251,8 @@ func (static staticBlackhole) Test(t *testing.T) {
 	assert.Comment("ping should get swallowed by blackhole")
 	assert.NonNil(static.PingCmd(t, "CA-1", "10.3.0.4"))
 
-	//FIXME
-	//assert.Program(regexp.MustCompile("drop"),
-	//	*Goes, "vnet", "show", "ip", "fib", "table",
-	//	"RA-2")
+	assert.Program(regexp.MustCompile("drop"),
+		*Goes, "fe1", "switch", "fib")
 
 	assert.Comment("Remove blackhole route")
 	static.ExecCmd(t, "RA-2", "ip", "route", "del", "blackhole",
@@ -289,9 +287,13 @@ func (static staticBlackhole) Test(t *testing.T) {
 	static.ExecCmd(t, "RA-1", "ip", "route", "del",
 		"blackhole", "192.168.0.0/25")
 	time.Sleep(1 * time.Second)
-	//FIXME
-	//assert.Program(*Goes, "vnet", "show", "ip", "fib", "table",
-	//	"RA-1")
+
+	status := assert.ProgramNonFatal(regexp.MustCompile("drop"),
+		*Goes, "fe1", "switch", "fib")
+	if status == true {
+		assert.Fatal("drop found")
+		return
+	}
 
 	assert.Comment("Now ping should work again")
 	assert.Nil(static.PingCmd(t, "CA-1", "192.168.0.2"))
